6a396a27cec38d3fa338dee3a42fb02ffa054525
TatyanaKabakova <omnitk56k@gmail.com>
2016-09-02 17:49:17 +0300

    upgraded from function-based consumers to class-based consumers

--- api/apps/chat/consumers.py
+++ api/apps/chat/consumers.py
@@ -1,12 +1,32 @@
 import json
 from channels.channel import Group
+from channels.generic import BaseConsumer
 
-def ws_connect(message):
-    Group('chat').add(message.reply_channel)
+# def ws_connect(message):
+#     Group('chat').add(message.reply_channel)
+#
+# def ws_message(message):
+#     Group('chat').send({'text': json.dumps({'message': message.content['text'],
+#                                             'sender': message.reply_channel.name})})
+#
+# def ws_disconnect(message):
+#     Group('chat').discard(message.reply_channel)
 
-def ws_message(message):
-    Group('chat').send({'text': json.dumps({'message': message.content['text'],
-                                            'sender': message.reply_channel.name})})
 
-def ws_disconnect(message):
-    Group('chat').discard(message.reply_channel)
+class MyConsumer(BaseConsumer):
+
+    method_mapping = {
+        "websocket.connect": "ws_connect",
+        "websocket.receive": "ws_message",
+        "websocket.disconnect": "ws_disconnect",
+    }
+
+    def ws_connect(self, message, **kwargs):
+        Group('chat').add(message.reply_channel)
+
+    def ws_message(self, message, **kwargs):
+        Group('chat').send({'text': json.dumps({'message': message.content['text'],
+                                                'sender': message.reply_channel.name})})
+
+    def ws_disconnect(self, message, **kwargs):
+        Group('chat').discard(message.reply_channel)
